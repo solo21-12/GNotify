ba94408eb7daeb3c671eea3175ef5a3b7d17e608
solo2112 <dawitabrham0021@gmail.com>
2024-02-02 15:04:26 +0300

    auth dtos defined

---
+++ gNotify-server/Dtos/RegisterRequest.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace gNotify_server.Dtos;
+
+public class RegisterRequest
+{
+    [Required,EmailAddress]
+    public string? Email { get; set; }
+    public string? Username { get; set; }
+    [Required]
+    public string? FirstName { get; set; }
+    public string? MiddleName { get; set; }
+    [Required]
+    public string? LastName { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Gender { get; set; }
+    
+    [Required,DataType(DataType.Password)]
+    public string? Password { get; set; }
+    
+    [Required,DataType(DataType.Password),Compare(nameof(Password),ErrorMessage = "Password doesn't match")]
+    public string? ConfirmPassword { get; set; }
+    
+}
